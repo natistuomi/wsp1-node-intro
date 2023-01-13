f67bb62705e0db44ea4744f21d0cb5554f833015
natistuomi <natalie.tuomi@elev.ga.ntig.se>
2023-01-13 08:50:37 +0100

    Del 1 & 2

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const nunjucks = require('nunjucks');
+const app = express();
+const port = 3000;
+
+app.get('/', (request, response) => {
+    response.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
+
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app,
+});
+
+app.get('/', async function (request, response, next) {
+    response.render('index.njk', {
+        message: 'Hello world!',
+        title: 'Nunjucks hello world',
+    });
+});
